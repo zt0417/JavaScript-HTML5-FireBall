14c5340ca89026757027934d3fe66f4c10806c20
Tong <zt04170@gmail.com>
2016-06-30 22:59:47 -0400

    Create main files

---
+++ js/shoot.js
@@ -0,0 +1,8 @@
+var backgroundColor = '#111';
+var gravity = 0.03;
+var fireballColor='#f73';
+
+var canvas=document.querySelector('canvas');
+var ctx=canvas.getContext('2d');
+ctx.fillStyle=backgrounfColor;
+ctx
