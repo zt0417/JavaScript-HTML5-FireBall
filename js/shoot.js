8a6b73da399aae88950f1edcb115c346d977d706
Tong <zt04170@gmail.com>
2016-07-01 23:44:38 -0400

    js

--- js/shoot.js
+++ js/shoot.js
@@ -4,5 +4,10 @@ var fireballColor='#f73';
 
 var canvas=document.querySelector('canvas');
 var ctx=canvas.getContext('2d');
-ctx.fillStyle=backgrounfColor;
-ctx
+
+(onresize = function(){
+  width = canvas.width = canvas.clientWidth;
+  height = canvas.height = canvas.clientHeight;
+  o = {x:Math.floor(width/2),y:Math.floor(height/2)};
+  edge = {top:-o.y,right:width-o.x,bottom:height-o.y,left:-o.x}
+})();
